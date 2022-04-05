011ef8c1f47a00bcca5ab1cc8fd34dcf6f2d5784
v.popov <v.popov@consaltica.ru>
2022-04-05 16:13:48 +0300

    Fix droolsCase

--- src/test/java/com/cognive/projects/casernkb/bpm/DroolsCaseBpmTest.java
+++ src/test/java/com/cognive/projects/casernkb/bpm/DroolsCaseBpmTest.java
@@ -1,7 +1,9 @@
 package com.cognive.projects.casernkb.bpm;
 
+import com.cognive.projects.casernkb.repo.BaseDictRepo;
 import com.prime.db.rnkb.model.*;
 import lombok.SneakyThrows;
+import org.assertj.core.api.Condition;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -17,9 +19,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.extension.mockito.CamundaMockito.registerCallActivityMock;
-import static org.camunda.bpm.extension.mockito.CamundaMockito.registerJavaDelegateMock;
+import static org.camunda.bpm.extension.mockito.CamundaMockito.*;
 import static org.camunda.bpm.extension.mockito.DelegateExpressions.autoMock;
+import static org.mockito.Mockito.when;
 
 @Deployment(resources = {
         "bpmn/cases/droolsCase.bpmn"
@@ -29,6 +31,7 @@ public class DroolsCaseBpmTest {
     public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
     private static final String payloadJsonAccepted = "{\"rules\":[\"01\",\"74\"],\"paymentId\":123}";
+    private static final String payloadJsonAccepted2 = "{\"rules\":[\"01\",\"74\"],\"paymentId\":123}";
     private static final String payloadJsonNoRules = "{\"rules\":[],\"paymentId\":1}";
 
     @Test
@@ -87,25 +90,80 @@ public class DroolsCaseBpmTest {
     }
 
     @Test
-    @Disabled
     @SneakyThrows
     public void Should_check_codes() {
         autoMock("bpmn/cases/droolsCase.bpmn");
 
         BaseDictionary crCode1 = new BaseDictionary();
         BaseDictionary crCode2 = new BaseDictionary();
+        BaseDictionary caseStatus1 = new BaseDictionary();
+        BaseDictionary caseType1 = new BaseDictionary();
 
         CaseRules cr1 = new CaseRules();
         CaseRules cr2 = new CaseRules();
 
         crCode1.setCode("01");
         crCode2.setCode("74");
+        caseStatus1.setCode("1");
+        caseType1.setCode("1");
 
         cr1.setCode(crCode1);
         cr2.setCode(crCode2);
 
         Case caseData = new Case();
-        caseData.setСaseRules(Arrays.asList(cr1, cr2));
+        caseData.setCaseRules(Arrays.asList(cr1, cr2));
+        caseData.setStatus(caseStatus1);
+        caseData.setCaseType(caseType1);
+
+        CaseOperation caseOperation = new CaseOperation();
+        caseOperation.setCaseId(caseData);
+
+        Payment payment = new Payment();
+        payment.setCaseOperationList(Collections.singletonList(caseOperation));
+
+        Map<String, Object> selectResult = new HashMap<>();
+        selectResult.put("payment", payment);
+
+        final FluentJavaDelegateMock selectOneDelegate = registerJavaDelegateMock("selectOneDelegate");
+        selectOneDelegate.onExecutionSetVariables(selectResult);
+
+        processEngineRule.manageDeployment(registerCallActivityMock("caseCreate")
+            .deploy(processEngineRule)
+        );
+
+        Map<String, Object> processParams = new HashMap<>();
+        processParams.put("payload", payloadJsonAccepted);
+
+        RuntimeService runtimeService = processEngineRule.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("droolsCase", processParams);
+
+        assertThat(processInstance)
+                .hasPassed("Activity_checkCodes", "Event_noNewCodes");
+    }
+
+
+    @Test
+    public void Should_save_new_rules() {
+        autoMock("bpmn/cases/droolsCase.bpmn");
+
+        BaseDictionary crCode1 = new BaseDictionary();
+        BaseDictionary crCode74 = new BaseDictionary();
+        BaseDictionary caseStatus1 = new BaseDictionary();
+        BaseDictionary caseType1 = new BaseDictionary();
+
+        CaseRules cr1 = new CaseRules();
+
+        crCode1.setCode("01");
+        crCode74.setCode("74");
+        caseStatus1.setCode("1");
+        caseType1.setCode("1");
+
+        cr1.setRuleId(crCode1);
+
+        Case caseData = new Case();
+        caseData.setCaseRules(Collections.singletonList(cr1));
+        caseData.setStatus(caseStatus1);
+        caseData.setCaseType(caseType1);
 
         CaseOperation caseOperation = new CaseOperation();
         caseOperation.setCaseId(caseData);
@@ -123,13 +181,85 @@ public class DroolsCaseBpmTest {
                 .deploy(processEngineRule)
         );
 
+        final BaseDictRepo baseDictionaryRepository = registerMockInstance(BaseDictRepo.class);
+        when(baseDictionaryRepository.getByBaseDictionaryTypeCodeAndCode(18, "74")).thenReturn(crCode74);
+
         Map<String, Object> processParams = new HashMap<>();
         processParams.put("payload", payloadJsonAccepted);
 
         RuntimeService runtimeService = processEngineRule.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("droolsCase", processParams);
 
+        Condition<Object> isCodes = new Condition<>(p -> {
+                Case checkCase = (Case)p;
+                return checkCase.getCaseRules().size() == 2 &&
+                        checkCase.getCaseRules().get(1).getRuleId().getCode().equals("74");
+            }, "isCaseRules size 2");
+
         assertThat(processInstance)
-                .hasPassed("Activity_checkCodes", "Event_noNewCodes");
+                .hasPassed("Activity_checkCodes", "Activity_saveRules")
+                .variables()
+                .hasEntrySatisfying("caseData", isCodes);
+    }
+
+    @Test
+    public void Should_save_comment() {
+        autoMock("bpmn/cases/droolsCase.bpmn");
+
+        BaseDictionary crCode1 = new BaseDictionary();
+        BaseDictionary crCode74 = new BaseDictionary();
+        BaseDictionary crCode75 = new BaseDictionary();
+        BaseDictionary caseStatus4 = new BaseDictionary();
+        BaseDictionary caseType1 = new BaseDictionary();
+
+        CaseRules cr1 = new CaseRules();
+
+        crCode1.setCode("01");
+        crCode74.setCode("74");
+        crCode75.setCode("75");
+        caseStatus4.setCode("4");
+        caseType1.setCode("1");
+
+        cr1.setRuleId(crCode1);
+
+        Case caseData = new Case();
+        caseData.setCaseRules(Collections.singletonList(cr1));
+        caseData.setStatus(caseStatus4);
+        caseData.setCaseType(caseType1);
+
+        CaseOperation caseOperation = new CaseOperation();
+        caseOperation.setCaseId(caseData);
+
+        Payment payment = new Payment();
+        payment.setCaseOperationList(Collections.singletonList(caseOperation));
+
+        Map<String, Object> selectResult = new HashMap<>();
+        selectResult.put("payment", payment);
+
+        final FluentJavaDelegateMock selectOneDelegate = registerJavaDelegateMock("selectOneDelegate");
+        selectOneDelegate.onExecutionSetVariables(selectResult);
+
+        processEngineRule.manageDeployment(registerCallActivityMock("caseCreate")
+                .deploy(processEngineRule)
+        );
+
+        final BaseDictRepo baseDictionaryRepository = registerMockInstance(BaseDictRepo.class);
+        when(baseDictionaryRepository.getByBaseDictionaryTypeCodeAndCode(18, "74")).thenReturn(crCode74);
+        when(baseDictionaryRepository.getByBaseDictionaryTypeCodeAndCode(18, "75")).thenReturn(crCode75);
+
+        Map<String, Object> processParams = new HashMap<>();
+        processParams.put("payload", payloadJsonAccepted2);
+
+        RuntimeService runtimeService = processEngineRule.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("droolsCase", processParams);
+
+        Condition<Object> isComment = new Condition<>(p -> {
+            Case checkCase = (Case)p;
+            return checkCase.getCaseCommentList().size() == 1 &&
+                    checkCase.getCaseCommentList().get(1).getComment().equals("74;75");
+        }, "isCaseRules size 2");
+
+        assertThat(processInstance)
+                .hasPassed("Activity_checkCodes", "Activity_saveComment");
     }
 }
